43dea24e2918cdccd1f157fdfdc1eb036159fb5d
Kerim Kurttepeli <kerimkurttepeli@kerims-mbp.mynetworksettings.com>
2023-05-05 10:10:27 -0400

    defined a class inside augment.py

--- source/tests/test_all.py
+++ source/tests/test_all.py
@@ -10,34 +10,36 @@ import numpy as np
 
 class TestMethods(unittest.TestCase):
     # UNIT TESTS
+    augmentum = augment.Augmentum
+
     def test_is_matrix_square(self):
         non_square_matrix = [[1, 1, 1], [1, 1, 1]]
-        assert augment.augment_image(non_square_matrix) == None
+        assert self.augmentum.augment_image(non_square_matrix) == None
 
     def test_rotate(self):
         square_matrix = [[1, 1, 1], [0, 0, 0], [0, 0, 0]]
-        rotate_matrix = augment.rotate(square_matrix)
+        rotate_matrix = self.augmentum.rotate(square_matrix)
         assert rotate_matrix == [[0, 0, 1], [0, 0, 1], [0, 0, 1]]
 
     def test_reflect(self):
         square_matrix = [[1, 0, 0], [1, 0, 0], [1, 0, 0]]
-        reflect_matrix = augment.reflect(square_matrix)
+        reflect_matrix = self.augmentum.reflect(square_matrix)
         assert reflect_matrix == [[0, 0, 1], [0, 0, 1], [0, 0, 1]]
 
     def test_right_shift(self):
         square_matrix = [[1, 1, 1], [1, 0, 0], [1, 0, 0]]
-        rs_matrix = augment.right_shift(square_matrix, 1)
+        rs_matrix = self.augmentum.right_shift(square_matrix, 1)
         assert rs_matrix == [[0, 1, 1], [0, 1, 0], [0, 1, 0]]
 
     def test_upsample_scaling(self):
         square_matrix = [[1, 0], [0, 1]]
-        us_matrix = augment.upsample_scaling(np.array(square_matrix)).tolist()
+        us_matrix = self.augmentum.upsample_scaling(np.array(square_matrix)).tolist()
         assert us_matrix == [[1, 1, 0, 0], [1, 1, 0, 0], [0, 0, 1, 1], [0, 0, 1, 1]]
 
     # INTEGRATION TEST
     def test_rotate_shift(self):
         square_matrix = [[0, 0, 0], [0, 0, 0], [1, 0, 1]]
-        rotate_matrix = augment.rotate(square_matrix)
-        rs_matrix = augment.right_shift(rotate_matrix, 1)
-        final_matrix = augment.reflect(rs_matrix)
+        rotate_matrix = self.augmentum.rotate(square_matrix)
+        rs_matrix = self.augmentum.right_shift(rotate_matrix, 1)
+        final_matrix = self.augmentum.reflect(rs_matrix)
         assert final_matrix == [[0, 1, 0], [0, 0, 0], [0, 1, 0]]
