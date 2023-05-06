21151d9bbe05ceb99abad67ee4b83b5327dd2c9e
Kerim Kurttepeli <kerimkurttepeli@kerims-mbp.mynetworksettings.com>
2023-05-05 21:16:24 -0400

    added comments for functions in main.py

--- Augmentum/main.py
+++ Augmentum/main.py
@@ -2,8 +2,15 @@ import numpy as np
 
 
 class Augmentum:
-    # Given an n x n 2D matrix (list of lists) representing an image, rotates the image by 90 degrees (clockwise).
     def rotate(self, matrix):
+        """
+        Given a binary image (list of lists) rotates it by 90 degrees (clockwise).
+
+        :param matrix: matrix representing the grayscale image
+        :type kind: list of lists
+        :return: grayscale image rotated by 90 degreees (clockwise)
+        :rtype: list of lists
+        """
         size = len(matrix)
         for i in range(size // 2 + size % 2):
             for j in range(size // 2):
@@ -14,16 +21,33 @@ class Augmentum:
                 matrix[i][j] = tmp
         return matrix
 
-    # Given an nxn 2D matrix (list of lists) reflect the image by its vertical central axis
     def reflect(self, matrix):
+        """
+        Given a binar image (list of lists) reflects it by its vertical central axis
+
+        :param matrix: matrix representing the grayscale image
+        :type kind: list of lists
+        :return: grayscale image reflected by its vertical central axis
+        :rtype: list of lists
+        """
         n = len(matrix)
         for i in range(n):
             for j in range(n // 2):
                 matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j]
         return matrix
 
-    # Given an nxn 2D matrix shifts the matrix to the right by x units (fills left values with 0)
     def right_shift(self, matrix, x):
+        """
+        Given a binary image (list of lists) shifts it to the right
+        by x units (fills left values with 0)
+
+        :param matrix: matrix representing the grayscale image
+        :type kind: list of lists
+        :param x: unit for right shift
+        :type kind: integer
+        :return: grayscale image shifted right by x units
+        :rtype: list of lists
+        """
         new_matrix = [[] for i in range(len(matrix))]
         for i in range(len(matrix)):
             for j in range(x):
@@ -32,15 +56,29 @@ class Augmentum:
                 new_matrix[i].append(matrix[i][j])
         return new_matrix
 
-    # Scales the size of the image by a factor of 2 by upsampling
     def upsample_scaling(self, matrix):
+        """
+        Given a binary image (list of lists) scales the size of it by a factor of 2 by upsampling
+
+        :param matrix: matrix representing the grayscale image
+        :type kind: list of lists
+        :return: grayscale image scaled by a factor of 2
+        :rtype: list of lists
+        """
         # smaller_img = bigger_img[::2, ::2]
         bigger_img = matrix.repeat(2, axis=0).repeat(2, axis=1)
         return bigger_img
 
-    # Given a binary image(list of lists) returns an image dataset (list of images) by appling
-    # image augmentation techniques
     def augment_image(self, image_matrix):
+        """
+        Given a binary image (list of lists) returns an image dataset (list of images)
+        by appling image augmentation techniques
+
+        :param matrix: matrix representing the grayscale image
+        :type kind: list of lists
+        :return: image dataset
+        :rtype: list of lists of lists
+        """
         if len(image_matrix) != len(image_matrix[0]):
             return None
         new_images = []
